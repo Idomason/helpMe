4bc48e27c627be0661059b15a7f0b69e22d054a3
Idomason <idomakevin@gmail.com>
2024-10-02 00:23:04 +0100

    work on the Question component

--- client/src/components/Questions/Questions.tsx
+++ client/src/components/Questions/Questions.tsx
@@ -2,11 +2,11 @@ import { ChevronDoubleDownIcon } from "@heroicons/react/24/solid";
 
 export default function Questions() {
   return (
-    <div className="my-3 flex cursor-pointer items-center justify-between rounded-sm bg-helpMe-950 px-2 py-2 shadow">
+    <div className="my-3 flex cursor-pointer items-center justify-between space-x-3 rounded-sm bg-helpMe-950 px-2 py-2 shadow">
       <span className="rounded-full bg-pink-400 px-3.5 py-1.5 font-medium text-white">
         1
       </span>
-      <p className="text-white">
+      <p className="md:text-md text-sm tracking-wider text-white">
         How can I verify my identity on the platform?
       </p>
       <ChevronDoubleDownIcon className="size-6 cursor-pointer text-pink-400" />
