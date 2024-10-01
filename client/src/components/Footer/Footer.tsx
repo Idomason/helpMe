17160fcdc276cc914bf0b953d10569f406fb0592
Idomason <idomakevin@gmail.com>
2024-10-02 00:16:21 +0100

    work on the footer component to make it more responsive

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <div className="mt-24 bg-black/90">
-      <div className="mx-auto grid h-full grid-cols-2 px-4 py-10 sm:grid-cols-3 md:w-11/12">
+    <div className="mt-24 overflow-hidden bg-black/90">
+      <div className="mx-auto grid h-full grid-cols-2 px-5 py-16 sm:grid-cols-3 md:w-11/12">
         {/* COLUMN 1 */}
-        <div className="col-span-1 mb-6 flex justify-between gap-10 sm:mb-0 sm:flex-col sm:gap-6">
+        <div className="col-span-3 mb-6 sm:col-span-1 sm:mb-0">
           <h1 className="font-bold tracking-widest text-white md:text-lg xl:text-2xl">
             HELP ME
           </h1>
-          <div className="flex space-x-4">
+          <div className="flex space-x-4 py-6">
             <Link to={"/"}>
               <Facebook className="cursor-pointer text-white transition-colors duration-300 hover:text-pink-400" />
             </Link>
@@ -24,73 +24,97 @@ export default function Footer() {
               <Youtube className="cursor-pointer text-white transition-colors duration-300 hover:text-pink-400" />
             </Link>
           </div>
-          <Link className="capitalize text-white" to={"/philanthropists"}>
+          <Link
+            className="lg:text-md text-sm capitalize text-white"
+            to={"/philanthropists"}
+          >
             philanthropists
           </Link>
         </div>
 
         {/* COLUMN 2 span 3 */}
         <div className="col-span-2 grid gap-y-16">
-          <div className="flex justify-between">
-            <div>
-              <Link className="block capitalize text-white" to={"/helpers"}>
-                helpers
-              </Link>
-              <Link className="block capitalize text-white" to={"/helpees"}>
-                helpees
-              </Link>
-            </div>
-            <div>
-              <Link className="block capitalize text-white" to={"/helpers"}>
-                helpers
-              </Link>
-              <Link className="block capitalize text-white" to={"/helpees"}>
-                helpees
-              </Link>
-            </div>
+          {/* COL 2 SEC 1 */}
+          <div className="flex justify-between justify-items-center sm:justify-between">
             <div>
               <Link
-                className="block py-1 capitalize text-white"
+                className="lg:text-md block text-sm capitalize text-white"
                 to={"/helpers"}
               >
                 helpers
               </Link>
               <Link
-                className="block py-1 capitalize text-white"
+                className="lg:text-md block text-sm capitalize text-white"
+                to={"/helpees"}
+              >
+                helpees
+              </Link>
+            </div>
+            <div>
+              <Link
+                className="lg:text-md block text-sm capitalize text-white"
+                to={"/helpers"}
+              >
+                helpers
+              </Link>
+              <Link
+                className="lg:text-md block text-sm capitalize text-white"
+                to={"/helpees"}
+              >
+                helpees
+              </Link>
+            </div>
+            <div>
+              <Link
+                className="lg:text-md block text-sm capitalize text-white"
+                to={"/helpers"}
+              >
+                helpers
+              </Link>
+              <Link
+                className="lg:text-md block text-sm capitalize text-white"
                 to={"/helpees"}
               >
                 helpees
               </Link>
             </div>
           </div>
-          <div className="grid grid-cols-3 items-center justify-between gap-10">
-            <div>
+
+          {/* COL 2 SEC 2 */}
+          <div className="grid grid-cols-3 items-center justify-between md:gap-10">
+            <div className="">
               <Link
-                className="block py-1 capitalize text-white"
+                className="lg:text-md block py-1 text-sm capitalize text-white"
                 to={"/requests"}
               >
                 requests
               </Link>
               <Link
-                className="block py-1 capitalize text-white"
+                className="lg:text-md block py-1 text-sm capitalize text-white"
                 to={"/allRequests"}
               >
                 all requests
               </Link>
+              <Link
+                className="lg:text-md block py-1 text-sm capitalize text-white"
+                to={"/contacts"}
+              >
+                Contacts
+              </Link>
             </div>
-            <div className="col-span-2 place-items-center">
+            <div className="col-span-2 place-items-start md:place-items-end">
               <p className="lg:text-md py-2 text-sm text-white">
                 Subscribe to our newsletter for updates
               </p>
               <div className="inline-flex">
                 <input
-                  className="rounded-l-sm border-none p-2 text-center text-helpMe-950 outline-none focus:ring-1 focus:ring-inset focus:ring-pink-400 xl:w-56"
+                  className="lg:text-md w-[9rem] rounded-l-sm border-none p-1.5 text-center text-sm text-helpMe-950 outline-none focus:ring-1 focus:ring-inset focus:ring-pink-400 sm:w-full sm:p-2 xl:w-56"
                   type="email"
                   name="subscribe"
                   id="subscribe"
                   placeholder="your_email@mail.com"
                 />
-                <button className="rounded-r-sm bg-pink-400 from-pink-600 to-rose-900 px-5 py-2 text-white transition-colors duration-300 ease-in-out hover:bg-gradient-to-br">
+                <button className="lg:text-md rounded-r-sm bg-pink-400 from-pink-600 to-rose-900 px-2 py-1.5 text-sm text-white transition-colors duration-300 ease-in-out hover:bg-gradient-to-br sm:px-5 sm:py-2">
                   Subscribe
                 </button>
               </div>
@@ -98,6 +122,10 @@ export default function Footer() {
           </div>
         </div>
       </div>
+      <hr className="mx-auto w-[90%]" />
+      <p className="mx-auto w-[90%] py-4 text-center capitalize text-gray-600">
+        All rights reserved @ Help me 2024
+      </p>
     </div>
   );
 }
