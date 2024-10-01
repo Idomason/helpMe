1a1e8f7a9c36682616114bb8c084c62830ec57b7
Idomason <idomakevin@gmail.com>
2024-10-01 14:33:21 +0100

    add the CTA component

--- client/src/components/Questions/Questions.tsx
+++ client/src/components/Questions/Questions.tsx
@@ -2,7 +2,7 @@ import { ChevronDoubleDownIcon } from "@heroicons/react/24/solid";
 
 export default function Questions() {
   return (
-    <div className="my-3 flex items-center justify-between rounded-sm bg-helpMe-950 px-2 py-2 shadow">
+    <div className="my-3 flex cursor-pointer items-center justify-between rounded-sm bg-helpMe-950 px-2 py-2 shadow">
       <span className="rounded-full bg-pink-400 px-3.5 py-1.5 font-medium text-white">
         1
       </span>
