0c029992d6079b0d573302cedf2c1092bb5b89f4
Idoma Ngbede <idomakevin@gmail.com>
2025-04-05 11:01:50 +0100

    work on Feature component responsiveness

--- client/src/components/FeatureCard/FeatureCard.tsx
+++ client/src/components/FeatureCard/FeatureCard.tsx
@@ -2,17 +2,17 @@ import { IFeature } from "../../utils/types";
 
 export default function FeatureCard({ feature }: IFeature) {
   return (
-    <div className="h-96 w-72 rounded-md bg-helpMe-950 px-4 shadow-2xl lg:w-80 xl:w-[19.4rem]">
-      <div className="flex flex-col items-center justify-center gap-4 px-4 py-8">
-        <span className="rounded-full bg-helpMe-700/70 px-4 py-2 font-bold text-helpMe-100 shadow-2xl xl:px-6 xl:py-4 xl:text-xl">
+    <div className="mb-2 h-full w-64 rounded-md bg-helpMe-950 px-4 shadow-2xl lg:w-72">
+      <div className="flex flex-col items-center justify-center gap-4 px-2 py-4">
+        <span className="rounded-full bg-helpMe-700/70 px-4 py-2 font-bold text-helpMe-100 shadow-2xl xl:text-xl">
           {feature.id}
         </span>
 
-        <h2 className="py-3 text-center font-semibold uppercase text-helpMe-100 lg:text-xl">
+        <h2 className="py-3 text-center font-semibold uppercase text-helpMe-100 lg:text-lg">
           {feature.title}
         </h2>
 
-        <p className="text-left text-helpMe-300 xl:text-[17px]">
+        <p className="text-left text-helpMe-300 xl:text-[16.5px]">
           {feature.body}
         </p>
       </div>
